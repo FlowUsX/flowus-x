15a8aa6b7a1e6692ec0d1cadfda319ca90dc872e
LetTTGACO <1225751694@qq.com>
2023-05-07 17:02:11 +0800

    feat(flowus-to-md): 新增page-to-md方法

--- packages/flowus-to-md/src/client.ts
+++ packages/flowus-to-md/src/client.ts
@@ -1,17 +1,34 @@
 import { PageBlocks } from '@flowusx/flowus-types'
 import { transform } from './utils/flowus'
 import { BlockType } from './const/flowus'
+import { ConfigOptions, FlowUsToMarkdownOptions } from './types'
+import { FlowUsClient } from '@flowusx/flowus-client'
+import { out } from '@flowusx/flowus-shared'
+import * as process from 'process'
 
 export class FlowUsToMarkdown {
-  constructor() {}
+  private flowusClient: FlowUsClient
+  private readonly config?: ConfigOptions
 
-  toMarkdownString(pageBlocks: PageBlocks): string {
+  constructor(options: FlowUsToMarkdownOptions) {
+    this.flowusClient = options.client
+    this.config = options.config
+    console.log(this.config)
+  }
+
+  public async pageToMarkdown(id: string) {
+    const pageBlocks = await this.flowusClient.getPageBlocks(id)
+    return this.toMarkdownString(pageBlocks)
+  }
+
+  public toMarkdownString(pageBlocks: PageBlocks): string {
     let mdString = ''
-    // 判断是文档页面还是数据表格页面
+    // 判断是是否是文档页面
     const blocksKeys = Object.keys(pageBlocks.blocks)
+    // 第一个节点标记了该Block的属性是文档还是其他
     const firstKey = blocksKeys[0]
     const firstValue = pageBlocks.blocks[firstKey]
-    if (firstValue.type === 0) {
+    if (firstValue.type === BlockType.Doc) {
       firstValue.subNodes.forEach((blockId) => {
         const block = pageBlocks.blocks[blockId]
         const type = block.type as BlockType
@@ -19,6 +36,8 @@ export class FlowUsToMarkdown {
       })
     } else {
       // 非页面Block，退出转换
+      out.err('类型错误', '非文档类型')
+      process.exit(1)
     }
 
     return mdString
--- packages/flowus-to-md/src/types/index.ts
+++ packages/flowus-to-md/src/types/index.ts
@@ -1,6 +1,16 @@
 import { BlockType } from '../const/flowus'
 import { Block, Blocks } from '@flowusx/flowus-types'
+import { FlowUsClient } from '@flowusx/flowus-client'
 
 export type Transform = {
   [key in BlockType]: (block: Block, blocks: Blocks) => string
+}
+
+export interface ConfigOptions {
+  saveChildPage?: boolean
+}
+
+export interface FlowUsToMarkdownOptions {
+  client: FlowUsClient
+  config?: ConfigOptions
 }
